b93f491aa6526865f9d26babee787ca7324f35fd
lokesh ingle <lokeshingle2020@gmail.com>
2023-01-26 21:02:09 +0530

    adding msg in sum.java

--- sum.java
+++ sum.java
@@ -2,6 +2,6 @@ class Sum{
     public static void main(String agrsp[]){
         int a = 4;
         int b = 5;
-        System.out.println(a + b);
+        System.out.println("The sum of:"a + b);
     }
 }
