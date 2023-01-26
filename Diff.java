ece4381fe4f9f1ee9c04c86761ff719d46f8e2fa
lokesh ingle <lokeshingle2020@gmail.com>
2023-01-26 22:26:26 +0530

    changes in Deff.java

--- Diff.java
+++ Diff.java
@@ -1,5 +1,6 @@
 class A{
     public static void main(String agr[]){
-        System.out.println("some")
+        System.out.println("something is wrong");
+        System.out.println("this is only for practice");
     }
 }
