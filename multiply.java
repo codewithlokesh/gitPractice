f321bed033dc83adaa49dbbac6f68e7b5721c29c
lokesh ingle <lokeshingle2020@gmail.com>
2023-01-26 21:50:26 +0530

    added multiply.java file

--- multiply.java
+++ multiply.java
@@ -2,6 +2,6 @@ class Sum{
     public static void main(String agrsp[]){
         int a = 4;
         int b = 5;
-        System.out.println("mul :"a*b);
+        System.out.println("multiply :"a*b);
     }
 }
